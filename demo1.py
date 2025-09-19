e42d39a2088b512a0e846c66740f7d81ff77b7b7
JustJuliesCode <66696117+JustJuliesCode@users.noreply.github.com>
2025-09-19 10:40:59 -0600

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,16 +1,5 @@
-# def add(a, b):
-#     return a + b
+def add(a, b):
+    return a + b
 
-# if __name__ == "__main__":
-#     print("Result:", add(2, 3))
-
-import unittest
-import demo1
-
-class TestAddFunction(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(demo1.add(2, 3), 5)
-        self.assertEqual(demo1.add(0, 0), 0)
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    print("Result:", add(2, 3))

d7f72026cca33352d7d7b715e0f1320000f5b0c6
JustJuliesCode <66696117+JustJuliesCode@users.noreply.github.com>
2025-09-18 18:50:21 -0600

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def process():
-    df = pd.read_csv('data.csv')
+    df = pd.read_csv('demo2_data.csv')
     avg = df['score'].mean()
     print(f"Average score: {avg:.2f}")
 

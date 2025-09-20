4a92976ffdcb9732e15b5f638183e19aff1ca125
JustJuliesCode <66696117+JustJuliesCode@users.noreply.github.com>
2025-09-19 20:23:50 -0600

    Update and rename demo1_test.py to demoAdd_test.py

--- demoAdd_test.py
+++ demoAdd_test.py
@@ -1,16 +1,16 @@
-# from demo1 import add
+# from demoAdd import add
 
 # def test_add():
 #     assert demo1.add(2, 3) == 5
 #     assert demo1.add(0, 0) == 0
 
 import unittest
-import demo1
+import demoAdd
 
 class TestAddFunction(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(demo1.add(2, 3), 5)
-        self.assertEqual(demo1.add(0, 0), 0)
+        self.assertEqual(demoAdd.add(2, 3), 5)
+        self.assertEqual(demoAdd.add(0, 0), 0)
 
 if __name__ == '__main__':
     unittest.main()

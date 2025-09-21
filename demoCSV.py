fe0023c22edd252741616262c732d3a7538dad9d
JustJuliesCode <66696117+JustJuliesCode@users.noreply.github.com>
2025-09-21 01:29:56 -0600

    Delete demoCSV.py

--- demoCSV.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def process():
-    df = pd.read_csv('demoAverage_data.csv')
-    avg = df['score'].mean()
-    print(f"Average score: {avg:.2f}")
-
-if __name__ == "__main__":
-    process()
